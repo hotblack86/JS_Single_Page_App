e41f699688749d2eeb8667ddb3f7a2bcccb49b6a
James Hotblack <james.c.hotblack@gmail.com>
2019-01-16 17:52:46 +0000

    Updated lib and spec. Jasmine can now run through browser.

---
+++ Single Page App/spec/MessageSpec.js
@@ -0,0 +1,6 @@
+describe('Message', function(){
+
+  it('Should allow user to enter a message', function(){
+    
+  })
+})
--- Single Page App/src/Message.js
+++ Single Page App/src/Message.js
@@ -1,10 +1,16 @@
 var messages = document.getElementById("messages");
 var textbox = document.getElementById("textbox");
 var button = document.getElementById("button");
+var newDate = new Date();
 
-button.addEventListener("click", function(){
+
+
+
+function add_and_display_message() {
      var newMessage = document.createElement("ul");
      newMessage.innerHTML = textbox.value;
-     messages.appendChild(newMessage);
+     messages.prepend(newMessage, newDate);
      textbox.value = "";
-});
+};
+
+button.addEventListener("click", add_and_display_message);
